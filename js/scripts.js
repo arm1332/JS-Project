40c4c24c1cc8a641609ccaffe27ed4ad1375495e
arm1332 <109930003+arm1332@users.noreply.github.com>
2022-09-19 11:33:49 -0400

    Added Pokemon to pokemonList
    Added name, type and height tags

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,6 @@
-pokemonList=[];
+let pokemonList=[
+  {name:"bulbasaur",type:["grass","poison"],height:"7"},
+  {name:"charmander", type:"fire",height:"6"},
+  {name:"pikachu",type:"electric",height:"4"}
+];
+console.log(pokemonList);
