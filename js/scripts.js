b8b41897fa69afb1b242777511cdf1713971116d
arm1332 <109930003+arm1332@users.noreply.github.com>
2022-09-20 12:29:25 -0400

    Added comments to code

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,11 @@
-let pokemonList=[
+let pokemonList=[ //making an array of objects
   {name:"bulbasaur",type:["grass","poison"],height:"0.7"},
   {name:"charmander", type:"fire",height:"0.6"},
   {name:"pikachu",type:"electric",height:"0.4"}
 ]
-
+// for loop to go through each item
 for(let i =0; i < pokemonList.length; i++ ) {
-  if (pokemonList[i].height > 0.7) {
+  if (pokemonList[i].height >= 0.7) { //if item is great or equal to height
     document.write(pokemonList[i].name + 'height:' + pokemonList[i].height +'-Wow, that/s big!' + '<br>');
   } else {
     document.write(pokemonList[i].name + 'height:' + pokemonList[i].height + '<br>');
