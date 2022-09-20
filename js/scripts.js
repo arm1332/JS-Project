589fc33448aa7bb927235bcf09b7fe3d9ef24f8c
arm1332 <109930003+arm1332@users.noreply.github.com>
2022-09-20 12:27:13 -0400

    Added code to pokemonList to set height

--- js/scripts.js
+++ js/scripts.js
@@ -2,5 +2,13 @@ let pokemonList=[
   {name:"bulbasaur",type:["grass","poison"],height:"0.7"},
   {name:"charmander", type:"fire",height:"0.6"},
   {name:"pikachu",type:"electric",height:"0.4"}
-];
-console.log(pokemonList);
+]
+
+for(let i =0; i < pokemonList.length; i++ ) {
+  if (pokemonList[i].height > 0.7) {
+    document.write(pokemonList[i].name + 'height:' + pokemonList[i].height +'-Wow, that/s big!' + '<br>');
+  } else {
+    document.write(pokemonList[i].name + 'height:' + pokemonList[i].height + '<br>');
+
+  }
+}
