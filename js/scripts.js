147d197dd8885c03165cb8be907b1e5dd6e71edd
arm1332 <109930003+arm1332@users.noreply.github.com>
2022-09-20 12:07:33 -0400

    Fixed Height of Pokemon

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 let pokemonList=[
-  {name:"bulbasaur",type:["grass","poison"],height:"7"},
-  {name:"charmander", type:"fire",height:"6"},
-  {name:"pikachu",type:"electric",height:"4"}
+  {name:"bulbasaur",type:["grass","poison"],height:"0.7"},
+  {name:"charmander", type:"fire",height:"0.6"},
+  {name:"pikachu",type:"electric",height:"0.4"}
 ];
 console.log(pokemonList);
