8c72e336c53702b3faabf6d76269ec060fee5c48
arm1332 <109930003+arm1332@users.noreply.github.com>
2022-09-30 11:56:08 -0400

    Updated Java Script with IIFE

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,44 @@
-let pokemonList=[ //making an array of objects
-  {name:"Bulbasaur",type:["grass","poison"],height:"0.7"},
-  {name:"Charmander", type:"fire",height:"0.6"},
-  {name:"Pikachu",type:"electric",height:"0.4"}
+//create variable pokemonReository
+let pokemonRepository =(function() {
+//making an array of objects
+  let pokemonList = [
+  {name:'Bulbasaur', height:'0.7', type:['grass','poison'] },
+  {name:'Charmander', height:'0.6', type:'fire'},
+  {name:'Pikachu', height:'0.4', type:'electric'}
 ]
-// for loop to go through each item
-for(let i =0; i < pokemonList.length; i++ ) {
-  if (pokemonList[i].height >= 0.7) { //if item is great or equal to height
-    document.write(pokemonList[i].name + ' height: ' + pokemonList[i].height +'-Wow, that\'s big!' + '<br>');
-  } else {
-    document.write(pokemonList[i].name + ' height: ' + pokemonList[i].height + '<br>');
+
+   function getAll() {
+     return pokemonList;
+   }
+    function add(pokemon) {
+      if (typeof pokemon === 'object' && 'name' in pokemon) {pokemonList.push(pokemon);
+                                                            }
+    }
+
+    return {
+      add: add,
+      getAll: getAll
+    }
+
+})()
+
+pokemonRepository.add({name:'Cubone', height:'0.4', type:'ground'});
+
+
+pokemonRepository.getAll().forEach(function(pokemon){
+
+
+  if (pokemon.height >= 0.7) { //if item is great or equal to height
+    document.write(pokemon.name + " height: "  + pokemon.height +'-Wow, that\'s big!' + '<br>');
 
   }
-}
+
+  else if (pokemon.height >= 0.5 && pokemon.height <= 0.7){
+     document.write(pokemon.name + " height: "  + pokemon.height +'-Wow, that\'s medium!' + '<br>');
+  }
+
+  else {
+  document.write(pokemon.name + " height: "  + pokemon.height +'-Wow, that\'s small!' + '<br>');
+
+  }
+})
