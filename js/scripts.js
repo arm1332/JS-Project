4a9d237e686b3f6673d72a81363ce76d381c9e86
arm1332 <109930003+arm1332@users.noreply.github.com>
2022-10-21 13:02:05 -0400

    Added new code to JS for pokemon list

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 //create variable pokemonReository
-let pokemonRepository =(function() {
+let pokemonRepository =(function(){
 //making an array of objects
   let pokemonList = [
   {name:'Bulbasaur', height:'0.7', type:['grass','poison'] },
@@ -10,25 +10,54 @@ let pokemonRepository =(function() {
    function getAll() {
      return pokemonList;
    }
+
     function add(pokemon) {
       if (typeof pokemon === 'object' && 'name' in pokemon) {pokemonList.push(pokemon);
                                                             }
     }
 
+      function addListItem(pokemon) {
+        let pokemonList = document.querySelector('.pokemon-list');
+
+        let listItem = document.createElement('li');
+
+        let button = document.createElement('button');
+        button.innerText = pokemon.name
+        button.classList.add('button-class');
+
+        listItem.appendChild(button);
+        pokemonList.appendChild(listItem);
+
+        button.addEventListener('click', function(event) {
+			showDetails(pokemon)
+		})
+	}
+
+	function showDetails(pokemon) {
+		console.log(pokemon);
+    }
+
     return {
       add: add,
-      getAll: getAll
+      getAll: getAll,
+     addListItem: addListItem,
+     showDetails: showDetails
     }
 
 })()
 
 pokemonRepository.add({name:'Cubone', height:'0.4', type:'ground'});
 
-
 pokemonRepository.getAll().forEach(function(pokemon){
 
+pokemonRepository.addListItem(pokemon);
+})
 
-  if (pokemon.height >= 0.7) { //if item is great or equal to height
+/*let container = document.querySelector('.container');
+container.innerHTML = '<button>Click Me</button>';
+console.log(container.innerHTML);*/
+
+ /* if (pokemon.height >= 0.7) { //if item is great or equal to height
     document.write(pokemon.name + " height: "  + pokemon.height +'-Wow, that\'s big!' + '<br>');
 
   }
@@ -41,4 +70,4 @@ pokemonRepository.getAll().forEach(function(pokemon){
   document.write(pokemon.name + " height: "  + pokemon.height +'-Wow, that\'s small!' + '<br>');
 
   }
-})
+}) */
